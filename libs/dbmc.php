6e68db34911b42cd3f8317de26bdfb322ff26407
sambhu Singh <sambhu.raj.singh@gmail.com>
2013-05-26 00:23:02 +0100

    commit the uncommitted

--- libs/dbmc.php
+++ libs/dbmc.php
@@ -41,71 +41,71 @@ class DBMC {
     $aDBC = $this->_appConfig->getDbDetails($dbName."_db");
     //print_r($aDBC);
     try {
-      $this->_oPDO = new PDO(              
+      $this->_oPDO = new PDO(
               'mysql:host='.$aDBC['dsn'].';dbname='.$aDBC['database'].';',
               $aDBC['username'],
               $aDBC['password'],
-              //Persistent conn r not closed at the end of the script but are cached and re-used when another script request a connection using the same credentials
+              //Persistent conn are not closed at the end of the script but are cached and re-used when another script request a connection using the same credentials
               //Persistent connection cache alllow to avoid the over of establishing a new conn and thus result in faster app
               array(PDO::ATTR_PERSISTENT => true) //Should I enable persisten connection
-             );
+      );
       $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch (PDOException $pe) {
       die ('PDO Exception '.$pe->getMessage());
     }
   }
 
-  public function bufferedMode(){
-        return $this->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, true);
-    }
+  public function bufferedMode() {
+    return $this->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, true);
+  }
 
-    public function errorCode(){
-        return $this->_oPDO->errorCode();
-    }
+  public function errorCode() {
+    return $this->_oPDO->errorCode();
+  }
 
-    public function errorInfo(){
-        return $this->_oPDO->errorInfo();
-    }
+  public function errorInfo() {
+    return $this->_oPDO->errorInfo();
+  }
 
-    public function exec($statement){
-        return $this->_oPDO->exec($statement);
-    }
+  public function exec($statement) {
+    return $this->_oPDO->exec($statement);
+  }
 
-    public function getAttribute($attribute){
-        return $this->_oPDO->getAttribute($attribute);
-    }
+  public function getAttribute($attribute) {
+    return $this->_oPDO->getAttribute($attribute);
+  }
 
-    public function getAvailableDrivers(){
-        return $this->_oPDO->getAvailableDrivers();
-    }
+  public function getAvailableDrivers() {
+    return $this->_oPDO->getAvailableDrivers();
+  }
 
-    public function getFoundRows(){
-        return $this->query('SELECT FOUND_ROWS()')->fetchColumn();
-    }
+  public function getFoundRows() {
+    return $this->query('SELECT FOUND_ROWS()')->fetchColumn();
+  }
 
-    public function rowsFound(){
-        return $this->query('SELECT FOUND_ROWS()')->fetchColumn() > 0 ? true : false;
-    }
+  public function rowsFound() {
+    return $this->query('SELECT FOUND_ROWS()')->fetchColumn() > 0 ? true : false;
+  }
 
-    public function lastInsertId($name=null){
-        return $this->_oPDO->lastInsertId($name);
-    }
+  public function lastInsertId($name=null) {
+    return $this->_oPDO->lastInsertId($name);
+  }
 
-    public function prepare($statement){
-        return $this->_oPDO->prepare($statement);
-    }
+  public function prepare($statement) {
+    return $this->_oPDO->prepare($statement);
+  }
 
-    public function query($statement){
-        return $this->_oPDO->query($statement);
-    }
+  public function query($statement) {
+    return $this->_oPDO->query($statement);
+  }
 
-    public function quote($string){
-        return $this->_oPDO->quote($string, $parameter_type);
-    }
+  public function quote($string) {
+    return $this->_oPDO->quote($string, $parameter_type);
+  }
 
-    public function rollBack(){
-        return $this->_oPDO->rollBack();
-    }
+  public function rollBack() {
+    return $this->_oPDO->rollBack();
+  }
 
   public function setAttribute($attribute, $value) {
     return $this->_oPDO->setAttribute($attribute, $value);
